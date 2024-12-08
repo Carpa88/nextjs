96384befc454212c9095cf0185372e02c1917549
Carpa88 <olenka_311@mail.ru>
2024-12-08 11:29:05 +0100

    added sructure

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Customer hage</div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Ivoices</div>
+  )
+}
+
+export default Page
